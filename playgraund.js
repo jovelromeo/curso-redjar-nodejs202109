228935396f44dc7e7731db78ea51c88619d42fd4
romeojovel <jovelromeo@gmail.com>
2021-09-14 09:04:07 -0300

    avance hasta operador ternario

--- playgraund.js
+++ playgraund.js
@@ -1,7 +1,27 @@
-const constante = "hola const";
-let variableLet = "hola let";
+// const constante = "hola const";
+// let variableLet = "hola let";
 
-constante = "modificada";
+// variableLet = "modificada";
 
-console.log(constante);
-console.log(variableLet);
+// console.log('La constante es:', constante);
+// console.log('La variable es:', variableLet);
+
+// const valor = null;
+// if (valor) {
+//     console.log('El valor es truthy');
+// } else {
+//     console.log('El valor es falsy');
+// }
+
+// let array1 = [{ attr: { valor: "VALOR!", fn: () => "Valor de fn" } }];
+// console.log(array1 && array1[0] && array1[0].attr && array1[0].attr.fn());
+// console.log(array1 && array1[0]?.attr?.fn());
+
+// let valor1 = "Rojo";
+// console.log(valor1 || "valor-por-defecto");
+
+
+let valor1 = "";
+let respuesta = (valor1 ? valor1 : "es falsy");
+console.log(respuesta);
+console.log(valor1 || "es falsy");
